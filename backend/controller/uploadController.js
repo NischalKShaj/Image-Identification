35a3fce207dba5a80f71a5a064b22468f1f99081
nischal <nischalkshaj5@gmail.com>
2025-04-20 22:36:52 +0530

    feat: created the express controller and the router

--- backend/controller/uploadController.js
+++ backend/controller/uploadController.js
@@ -2,10 +2,27 @@
 
 // importing the required modules
 const dotenv = require("dotenv");
+const child = require("child_process");
 dotenv.config();
 
 // creating the controller object
-const uploadController = {};
+const uploadController = {
+  // controller for predicting the image
+  predict: async (req, res) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: "No Image Found" });
+      }
+
+      // for setting up the child process for the image prediction
+      const spawn = child.spawn("python", []);
+      res.status(200).json({ prediction: "Prediction" });
+    } catch (error) {
+      console.error("error from the predict controller", error);
+      res.status(500).json({ error: error });
+    }
+  },
+};
 
 // exporting the modules
 module.exports = uploadController;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,12 +2,18 @@
 
 // importing the required modules
 const express = require("express");
+const uploadController = require("../controller/uploadController");
+const multer = require("multer");
+
+// setting up the multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // setting the route
 const router = express.Router();
 
 // router for uploading the image for processing
-router.post("/uploads");
+router.post("/uploads", upload.single("image"), uploadController.predict);
 
 // exporting the routes
 module.exports = router;
